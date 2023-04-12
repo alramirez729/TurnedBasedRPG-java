4d33ad2b75d09f4298ec671113a758e638d09c30
alramirez729 <130582806+alramirez729@users.noreply.github.com>
2023-04-12 13:30:56 -0400

    Add files via upload
    This is an outline of all the files

---
+++ PlayerChar.java
@@ -0,0 +1,12 @@
+public abstract class PlayerChar {
+    private String playerType;
+    private String name;
+    private int hp;
+    private int mp;
+
+    public PlayerChar(String name, String type){
+        this.name = name;
+        this.playerType = type;
+    }
+
+}
