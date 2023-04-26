5301bec929808da22b9e288310ae8ca59698137e
russellwfortin <125509482+russellwfortin@users.noreply.github.com>
2023-04-26 12:26:31 -0400

    Update UndeadBeast.java

--- src/com/rpgbattle/UndeadBeast.java
+++ src/com/rpgbattle/UndeadBeast.java
@@ -1,95 +1,41 @@
+package com.rpgbattle;
 import java.util.Random;
 
-public class UndeadBeast extends PlayerCharacter
+public class UndeadBeast extends Character
 {
-    private int maxHP;
-    private int currentHP;
-    private int strength;
-    private String name;
-    
-    public String getName() {
-        return name;
-    }
   
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getMaxHP() {
-        return maxHP;
-    }
-
-    public void setMaxHP(int maxHP) {
-        this.maxHP = maxHP;
-    }
-
-    public int getCurrentHP() {
-        return currentHP;
-    }
-
-    public void setCurrentHP(int currentHP) {
-        this.currentHP = ( currentHP < 1 ) ? 0 : currentHP;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-   
-
-   
 public UndeadBeast()
    {
-    super(500, 500, 100, "Undead Beast");
-    maxHP = 500;
-    currentHP = 500;
-    strength = 160;
-    name = "Undead Beast";
-    
-
+    super(500, 500, 21, "Undead Beast");
    }
+
    public UndeadBeast( int maxH, int currentH, int stren, String nombre)
    {
     super(maxH, currentH, stren, nombre); 
-    maxHP = ( maxH < 1 ) ? 1 : maxH;
-    currentHP = ( currentH < 1 ) ? 1 : currentH;
-    strength = ( stren < 1 ) ? 1 : stren;
-    name = nombre;
    } 
 
    @Override
-   public void beHealed( int heal){
-       System.out.printf("\n%s receives no healing!", getName());
-   } 
-   
-   
-   @Override
-   public int swing()
-   {
-      int hit;
-      Random rand = new Random();
-      hit=(int) ((10-(rand.nextInt(4)))*getStrength()*0.13);
-      return hit;
-   } // this is math to swing against one person
-
-   public void toSwing(  PlayerCharacter d)
+   public void attack(  Character d)
     {
-      System.out.printf("\n%s takes a swing!", getName());
-      d.setCurrentHP(d.getCurrentHP()-this.swing());
+       int hit;
+       Random rand = new Random();
+       hit=(int) ((10-(rand.nextInt(4)))*getStrength());
+       d.setCurrentHP(d.getCurrentHP()-hit);
+       System.out.printf("\n%s attacks %s for %d damage! %s", getName(), d.getName(), hit, d.toString());
     }
 
-   // return String representation of CommissionEmployee object
    @Override
    public String toString()
    {
       return String.format( "\n%s: %d/%d HP max", getName(),
          getCurrentHP(), getMaxHP()) ;
-   } // end method toString
+   }       
 
+   @Override
+   public void move(Character[] allies, Character[] enemies)
+   {
+        Random rand = new Random();
+        attack(allies[rand.nextInt(4)]);
+    }
+    }
 
-        
-} // end class 
