39f3abdab36f8d2747cfbbc96d0bc260dfc5fb17
russellwfortin <125509482+russellwfortin@users.noreply.github.com>
2023-04-26 12:20:57 -0400

    Update Cleric.java

--- src/com/rpgbattle/Cleric.java
+++ src/com/rpgbattle/Cleric.java
@@ -1,23 +1,12 @@
+package com.rpgbattle;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Cleric extends PlayerCharacter implements HealCaster
+public class Cleric extends Character implements HealCaster
 {
-    private int maxHP;
-    private int currentHP;
-    private int strength;
     private int currentMP;
     private int maxMP;
     private int intelligence;
-    private String name;
-
-
-    public String getName() {
-        return name;
-    }
-  
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getIntelligence() {
     return intelligence;
@@ -27,30 +16,6 @@ public class Cleric extends PlayerCharacter implements HealCaster
     this.intelligence = intelligence;
 }
    
-    public int getMaxHP() {
-        return maxHP;
-    }
-
-    public void setMaxHP(int maxHP) {
-        this.maxHP = maxHP;
-    }
-
-    public int getCurrentHP() {
-        return currentHP;
-    }
-
-    public void setCurrentHP(int currentHP) {
-        this.currentHP = ( currentHP < 1 ) ? 0 : currentHP;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
     public int getCurrentMP() {
         return currentMP;
     }
@@ -67,98 +32,96 @@ public class Cleric extends PlayerCharacter implements HealCaster
         this.maxMP = maxMP;
     }
    
-
-   
    public Cleric()
    {
-    super(250, 250, 70, "Cleric");
-    maxHP = 250;
-    currentHP = 250;
-    strength = 70;
+    super(250, 250, 9, "Cleric");
     maxMP = 100;
     currentMP = 100;
-    intelligence = 100;
-    name = "Cleric";
+    intelligence = 33;
 
    }
    public Cleric( int maxH, int currentH, int maxM, int currentM, int stren, int intel, String nombre)
    {
     super(maxH, currentH, stren, nombre);
-    maxHP = maxH;
-    currentHP = currentH;
-    strength = stren;
     maxMP = ( maxH < 20 ) ? 20 : maxM;
     currentMP = ( currentM < 0 ) ? 0 : currentM;
     intelligence = ( intel < 1 ) ? 1 : intel;
-    name = nombre;
    } 
 
-   @Override
-   public void beHealed(int heal){
-    if (getCurrentHP()!=0)
-    {
-    int FixedHeal = ( heal + getCurrentHP() >= getMaxHP() ) ? getMaxHP() : heal; 
-    setCurrentHP(FixedHeal);
-    System.out.printf("\n%s received healing!", getName());
-    }
-    else
-    {
-     System.out.printf("\n%s is dead and cannot be healed.", getName());
-    }
-   } //this method is how much an enemy would receive, and will need to be done again for each enemy
-
-   public void toCastHeal(  PlayerCharacter[] allies)
+@Override
+   public void heal(  Character[] allies)
     {
         if (getCurrentMP()>=20)
     {
        System.out.printf("\n%s casts Heal on all allies!", getName());
        setCurrentMP(getCurrentMP()-20);  
-       for ( PlayerCharacter currentAlly : allies )
+       for ( Character currentAlly : allies )
        {
-        currentAlly.beHealed(currentAlly.getCurrentHP()+this.castHeal());
+        if (currentAlly.getCurrentHP()!=0)
+        {
+        int heal;
+        Random rand = new Random();
+        heal=(int) ((20-(rand.nextInt(4)))*getIntelligence());   
+        int FixedHeal = ( heal + currentAlly.getCurrentHP() >= currentAlly.getMaxHP() ) ? currentAlly.getMaxHP() : heal; 
+        currentAlly.setCurrentHP(FixedHeal);
         System.out.printf("%s", currentAlly.toString());
+        }
+        else
+        {
+         System.out.printf("\n%s is dead and cannot be healed.", currentAlly.getName());
+        }
        }
     }
     else
        {
         System.out.printf("\n%s needs 20 MP to cast Heal!", getName()); 
        }  
+       System.out.println();
     }
-
-    @Override
-   public int castHeal(){
-    //if (getCurrentMP()>=20)
-   // {
-     //  setCurrentMP(getCurrentMP()-20);  
-       int heal;
-       Random rand = new Random();
-       heal=(int) ((20-(rand.nextInt(4)))*getIntelligence()*0.33);
-       return heal;
-  //  }
-    //else
-   // {
-      //  System.out.println(""); 
-      //  System.out.print("Cleric needs 20 MP to cast Heal!"); 
-    }  
-      // return 0;  
- //  } //this method is how much an enemy would receive, and will need to be done again for each enemy
    
-   @Override
-   public int swing()
-   {
+@Override
+   public void attack(  Character d)
+    {
        int hit;
        Random rand = new Random();
-       hit=(int) ((10-(rand.nextInt(4)))*getStrength()*0.13);
-       return hit;
-   } // this is math to swing against one person
-
-   public void toSwing(  PlayerCharacter d)
-    {
-      System.out.printf("\n%s takes a swing!", getName());
-      d.setCurrentHP(d.getCurrentHP()-this.swing());
+       hit=(int) ((10-(rand.nextInt(4)))*getStrength());
+       d.setCurrentHP(d.getCurrentHP()-hit);
+       System.out.printf("\n%s attacks %s for %d damage! %s", getName(), d.getName(), hit, d.toString());
     }
 
-   // return String representation of CommissionEmployee object
+   @Override
+   public String toString()
+   {
+      return String.format( "\n%s: %s, %d/%d MP max",
+         getName(), super.toString(), 
+         getCurrentMP(), getMaxMP()); 
+        
+   }
+
+   @Override
+   public void move(Character[] allies, Character[] enemies)
+   {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n1. Fight");
+        System.out.println("2. Heal");
+        System.out.print("Choice: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1: // Fight
+                System.out.print("\nHere are the enemies to fight:");
+                for (int i = 0; i < enemies.length; i++)
+                   System.out.printf("%s (Enter %d to attack)", enemies[i].toString(), i+1);
+                System.out.print("\nWhich one do you want to fight?");
+                int en = input.nextInt()-1;   
+                attack(enemies[en]);
+
+                break;
+            case 2: 
+                heal(allies);
+        } // end switch
+    }
+
+} 
    @Override
    public String toString()
    {
