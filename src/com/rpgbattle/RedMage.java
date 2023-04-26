54aefbd2f72e800c832726d85d9ca1d51e15fb75
russellwfortin <125509482+russellwfortin@users.noreply.github.com>
2023-04-26 12:24:57 -0400

    Update RedMage.java

--- src/com/rpgbattle/RedMage.java
+++ src/com/rpgbattle/RedMage.java
@@ -1,22 +1,12 @@
+package com.rpgbattle;
 import java.util.Random;
+import java.util.Scanner;
 
-public class RedMage extends PlayerCharacter implements DamageCaster, HealCaster
+public class RedMage extends Character implements DamageCaster, HealCaster
 {
-    private int maxHP;
-    private int currentHP;
-    private int strength;
     private int currentMP;
     private int maxMP;
     private int intelligence;
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-  
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getIntelligence() {
     return intelligence;
@@ -26,31 +16,6 @@ public class RedMage extends PlayerCharacter implements DamageCaster, HealCaster
     this.intelligence = intelligence;
 }
    
-    public int getMaxHP() {
-        return maxHP;
-    }
-
-    public void setMaxHP(int maxHP) {
-        this.maxHP = maxHP;
-    }
-
-    public int getCurrentHP() {
-        return currentHP;
-    }
-
-    public void setCurrentHP(int currentHP) {
-        this.currentHP = ( currentHP < 1 ) ? 0 : currentHP;
-        //this.currentHP = ( currentHP < 1 && currentHP > getMaxHP() ) ? 0 : currentHP;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
     public int getCurrentMP() {
         return currentMP;
     }
@@ -71,60 +36,33 @@ public class RedMage extends PlayerCharacter implements DamageCaster, HealCaster
    
    public RedMage()
    {
-    super(250, 250, 70, "Red Mage");
-    maxHP = 250;
-    currentHP = 250;
-    strength = 100;
-    maxMP = 50;
-    currentMP = 50;
-    intelligence = 100;
-    name = "Red Mage";
+    super(250, 250, 13, "Red Mage");
+    maxMP = 60;
+    currentMP = 60;
+    intelligence = 33;
 
    }
    public RedMage( int maxH, int currentH, int maxM, int currentM, int stren, int intel, String nombre)
    {
       super(maxH, currentH, stren, nombre);
-      maxHP = maxH;
-      currentHP = currentH;
-      strength = stren;
       maxMP = ( maxH < 20 ) ? 20 : maxM;
       currentMP = ( currentM < 0 ) ? 0 : currentM;
       intelligence = ( intel < 1 ) ? 1 : intel;
-      name = nombre;
-   } 
-
-   @Override
-   public void beHealed(int heal){
-       if (getCurrentHP()!=0)
-       {
-       int FixedHeal = ( heal + getCurrentHP() >= getMaxHP() ) ? getMaxHP() : heal; 
-       setCurrentHP(FixedHeal);
-       System.out.printf("\n%s received healing!", getName());
-       }
-       else
-       {
-        System.out.printf("\n%s is dead and cannot be healed.", getName());
-       }
    } 
    
-    @Override
-   public int castThunder(){
-      
-       int thunder;
-       Random rand = new Random();
-       thunder=(int) ((20-(rand.nextInt(4)))*getIntelligence()*0.33);
-       return thunder;
-   } //this method is how much an enemy would receive, and will need to be done again for each enemy
-
-   public void toThunder(  PlayerCharacter[] enemies)
+   @Override
+   public void thunder(  Character[] enemies)
     {
         if (getCurrentMP()>=20)
     {
        System.out.printf("\n%s casts Thunder on all enemies!", getName());
        setCurrentMP(getCurrentMP()-20);  
-       for ( PlayerCharacter currentEnemy : enemies )
+       for ( Character currentEnemy : enemies )
        {
-        currentEnemy.setCurrentHP(currentEnemy.getCurrentHP()-this.castThunder());
+        int thunder;
+        Random rand = new Random();
+        thunder=(int) ((20-(rand.nextInt(4)))*getIntelligence());
+        currentEnemy.setCurrentHP(currentEnemy.getCurrentHP()-thunder);
         System.out.printf("%s", currentEnemy.toString());
        }
     }
@@ -132,48 +70,50 @@ public class RedMage extends PlayerCharacter implements DamageCaster, HealCaster
        {
         System.out.printf("\n%s needs 20 MP to cast Thunder!", getName()); 
        }  
+       System.out.println();
     }
     
-    public void toCastHeal(  PlayerCharacter[] allies)
-    {
-        if (getCurrentMP()>=20)
-    {
-       System.out.printf("\n%s casts Heal on all allies!", getName());
-       setCurrentMP(getCurrentMP()-20);  
-       for ( PlayerCharacter currentAlly : allies )
-       {
-        currentAlly.beHealed(currentAlly.getCurrentHP()+this.castHeal());
-        System.out.printf("%s", currentAlly.toString());
-       }
-    }
-    else
-       {
-        System.out.printf("\n%s needs 20 MP to cast Heal!", getName()); 
-       }  
-    }
-
     @Override
-    public int castHeal(){
-       int heal;
-       Random rand = new Random();
-       heal=(int) ((20-(rand.nextInt(4)))*getIntelligence()*0.33);
-       return heal;
-    }  
+    public void heal(  Character[] allies)
+     {
+         if (getCurrentMP()>=20)
+     {
+        System.out.printf("\n%s casts Heal on all allies!", getName());
+        setCurrentMP(getCurrentMP()-20);  
+        for ( Character currentAlly : allies )
+        {
+         if (currentAlly.getCurrentHP()!=0)
+         {
+         int heal;
+         Random rand = new Random();
+         heal=(int) ((20-(rand.nextInt(4)))*getIntelligence());   
+         int FixedHeal = ( heal + currentAlly.getCurrentHP() >= currentAlly.getMaxHP() ) ? currentAlly.getMaxHP() : heal; 
+         currentAlly.setCurrentHP(FixedHeal);
+         System.out.printf("%s", currentAlly.toString());
+         }
+         else
+         {
+          System.out.printf("\n%s is dead and cannot be healed.", currentAlly.getName());
+         }
+        }
+     }
+     else
+        {
+         System.out.printf("\n%s needs 20 MP to cast Heal!", getName()); 
+        }  
+        System.out.println();
+     }
     
-    @Override
-    public int swing()
-   {
-       int hit;
-       Random rand = new Random();
-       hit=(int) ((10-(rand.nextInt(4)))*getStrength()*0.13);
-       return hit;
-   } // this is math to swing against one person
 
-   public void toSwing(  PlayerCharacter d)
-    {
-      System.out.printf("\n%s takes a swing!", getName());
-      d.setCurrentHP(d.getCurrentHP()-this.swing());
-    }
+    @Override
+    public void attack(  Character d)
+     {
+        int hit;
+        Random rand = new Random();
+        hit=(int) ((10-(rand.nextInt(4)))*getStrength());
+        d.setCurrentHP(d.getCurrentHP()-hit);
+        System.out.printf("\n%s attacks %s for %d damage! %s", getName(), d.getName(), hit, d.toString());
+     }
 
    @Override
    public String toString()
@@ -181,6 +121,34 @@ public class RedMage extends PlayerCharacter implements DamageCaster, HealCaster
       return String.format( "\n%s: %s, %d/%d MP max",
          getName(), super.toString(), 
          getCurrentMP(), getMaxMP()); 
-        
-   } // end method toString
+   } 
+
+   @Override
+   public void move(Character[] allies, Character[] enemies)
+   {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n1. Fight");
+        System.out.println("2. Thunder");
+        System.out.println("3. Heal");
+        System.out.print("Choice: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1: // Fight
+                System.out.print("\nHere are the enemies to fight:");
+                for (int i = 0; i < enemies.length; i++)
+                   System.out.printf("%s (Enter %d to attack)", enemies[i].toString(), i+1);
+                System.out.print("\nWhich one do you want to fight?");
+                int en = input.nextInt()-1;   
+                attack(enemies[en]);
+
+                break;
+            case 2: 
+                thunder(enemies);
+                break;
+            case 3: 
+                heal(allies);
+                break;
+        } // end switch
+    }
+
 } // end class 
