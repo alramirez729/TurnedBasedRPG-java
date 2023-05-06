28b3c9e880803c2dc0e940cda8f1d76998e21184
alramirez729 <130582806+alramirez729@users.noreply.github.com>
2023-05-05 20:29:49 -0400

    Update GameGUI.java
    added several layers to the GUI including:
    - font
    -720p res
    - getUserInputAsInt
    -handle some GUI exceptions 
    - close game confirmation
    - center window on any screen

--- src/com/rpgbattle/GameGUI.java
+++ src/com/rpgbattle/GameGUI.java
@@ -1,22 +1,31 @@
 package com.rpgbattle;
 
-import java.awt. *;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-
 import java.io.*;
 
 public class GameGUI extends JFrame {
 
-    private JTextArea textArea;
-    private JTextField inputField;
+    private final JTextArea textArea;
 
     public GameGUI() {
         super("RPG FIGHT GAME");
-        setSize(600, 400); //resolution of popout Window
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280, 720); //resolution of popout Window
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(screenSize.width / 2 - getWidth() / 2, screenSize.height / 2 - getHeight() / 2);
+
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // Prevent the default close operation
+
+
+        // Add a window listener to handle window closing event
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                confirmClose();
+            }
+        });
 
         JPanel panel = new JPanel(new BorderLayout());
         getContentPane().add(panel);
@@ -25,18 +34,8 @@ public class GameGUI extends JFrame {
         textArea.setBackground(Color.BLACK); //sets the color of the popout "terminal" to black
         textArea.setForeground(Color.WHITE);
         textArea.setEditable(false);
+        textArea.setFont(new Font("Times New Roman", Font.BOLD, 16)); // Set custom font here
         JScrollPane scrollPane = new JScrollPane(textArea);
-
-        inputField = new JTextField();
-        inputField.setBounds(20, 450, 450, 30);
-        inputField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String userInput = inputField.getText();
-                // Use the user input in the game
-            }
-        });
-        add(inputField);
 
         panel.add(scrollPane, BorderLayout.SOUTH);
         getContentPane().add(scrollPane);
@@ -47,14 +46,37 @@ public class GameGUI extends JFrame {
         setVisible(true);
     }
 
-    public JTextArea getTextArea(){
+    public JTextArea getTextArea() {
         return textArea;
     }
 
+    public static int getUserInputAsInt(String message) {
+        String input = JOptionPane.showInputDialog(null, message);
+        if (input != null && !input.isEmpty()) {
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                // Handle invalid input here (e.g., display an error message)
+                e.printStackTrace();
+            }
+        } else {
+            confirmClose(); // Display the confirmation dialog when the user cancels
+        }
+        return 0; // Default value if user cancels or enters invalid input
+    }
+
+    private static void confirmClose() {
+        int confirm = JOptionPane.showConfirmDialog(null,
+                "Are you sure you want to close the game?\nAll progress will be lost.",
+                "Confirm Close", JOptionPane.YES_NO_OPTION);
+
+        if (confirm == JOptionPane.YES_OPTION) {
+            System.exit(0); // Exit the program
+        }
+    }
 
     public static class CustomOutputStream extends OutputStream {
-        private JTextArea textArea;
-        private long delay = 10; // delay in milliseconds
+        private final JTextArea textArea;
 
         public CustomOutputStream(JTextArea textArea) {
             this.textArea = textArea;
@@ -64,12 +86,14 @@ public class GameGUI extends JFrame {
             // Redirect output from System.out to the text area
             textArea.append(String.valueOf((char) b));
             textArea.setCaretPosition(textArea.getDocument().getLength());
-
             try {
+                // delay in milliseconds
+                long delay = 25;
                 Thread.sleep(delay);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         }
     }
+
 }
